fe34453980f5196883be30324d52e2b2157a3a5f
bartlomiejwyszecki <bartlomiej.wyszecki2@gmail.com>
2021-03-05 11:47:37 +0100

    Navbar changes: now the brand name is showing on scroll bigger than 130pxs, font changed to serif - 'Playfair Dispaly'.

--- src/app/components/navbar/navbar.component.ts
+++ src/app/components/navbar/navbar.component.ts
@@ -1,15 +1,30 @@
-import { Component, OnInit } from '@angular/core';
+import { DOCUMENT } from '@angular/common';
+import { ChangeDetectionStrategy, Component, OnDestroy, Inject, HostListener } from '@angular/core';
+import { fromEvent, Subject } from 'rxjs';
+import { takeUntil } from 'rxjs/operators';
 
 @Component({
   selector: 'app-navbar',
   templateUrl: './navbar.component.html',
-  styleUrls: ['./navbar.component.scss']
+  styleUrls: ['./navbar.component.scss'],
+  changeDetection: ChangeDetectionStrategy.OnPush
+
 })
-export class NavbarComponent implements OnInit {
+export class NavbarComponent implements OnDestroy {
+  check: boolean;
 
-  constructor() { }
+  constructor(
+    @Inject (DOCUMENT) private document: Document
+  ) { }
 
-  ngOnInit(): void {
+  @HostListener("document:scroll", [])
+  onScroll() {
+    const offset = this.document.documentElement.scrollTop ||
+      this.document.body.scrollTop || 0;
+    console.log(offset);
+    offset > 130 ? this.check = true : this.check = false;
   }
 
+  ngOnDestroy(): void {
+  }
 }
