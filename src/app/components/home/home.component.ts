144b754fa17acc5626671d3a17a783011684d530
bartlomiejwyszecki <bartlomiej.wyszecki2@gmail.com>
2021-03-15 01:09:00 +0100

    Added content to home component, added routing to specific fragment inside homeComponent.

--- src/app/components/home/home.component.ts
+++ src/app/components/home/home.component.ts
@@ -1,15 +1,26 @@
+import { DatePipe } from '@angular/common';
 import { Component, OnInit } from '@angular/core';
+import { ActivatedRoute } from '@angular/router';
 
 @Component({
   selector: 'app-home',
   templateUrl: './home.component.html',
-  styleUrls: ['./home.component.scss']
+  styleUrls: ['./home.component.scss'],
+  providers: [DatePipe]
 })
 export class HomeComponent implements OnInit {
+  myDate: string | Date = new Date();
+  fragment: string;
 
-  constructor() { }
-
-  ngOnInit(): void {
+  constructor(private datePipe: DatePipe, private route: ActivatedRoute) {
+    this.myDate = this.datePipe.transform(this.myDate, 'yyyy-MM-dd');
   }
 
+  ngOnInit() {
+    this.route.fragment.subscribe(fragment => {      
+      if (fragment) {
+        document.querySelector('#' + fragment).scrollIntoView();
+      }
+    });
+  }
 }
